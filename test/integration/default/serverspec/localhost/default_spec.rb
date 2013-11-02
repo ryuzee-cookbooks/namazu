d2990d966349223378c6fb6e75efd7a950d04c9c
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-11-03 08:01:14 +0900

    add tests

---
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -0,0 +1,24 @@
+require 'serverspec'
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+RSpec.configure do |c|
+  c.before :all do
+    c.os = backend(Serverspec::Commands::Base).check_os
+  end
+  c.path = "/sbin:/user/sbin"
+end
+
+describe package('nkf') do
+  it { should be_installed }
+end
+
+describe file('/usr/local/bin/kakasi') do
+  it { should be_file }
+  it { should be_mode 755 }
+end
+
+describe file('/usr/local/bin/mknmz') do
+  it { should be_file }
+  it { should be_mode 755 }
+end
