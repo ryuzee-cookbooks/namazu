41d68144969b56c8a439aae5b3e3120cbb432d3c
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-11-24 06:32:09 +0900

    clarify license

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,4 @@
 default["namazu"]["version"] = "2.0.21"
 default["namazu"]["kakasi_version"] = "2.3.4"
+
+# vim: filetype=ruby.chef
--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "namazu"
 maintainer       "ryuzee"
 maintainer_email "ryuzee@gmail.com"
-license          "All rights reserved"
+license          "MIT"
 description      "Installs/Configures namazu"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.3"
